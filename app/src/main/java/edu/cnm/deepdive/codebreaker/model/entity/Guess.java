008f9cec7245fb1291a7fe04c74632b08658a5ca
Nick Bennett <nick@nickbenn.com>
2022-02-09 14:50:11 -0700

    Rudimentary game play implemented.

--- app/src/main/java/edu/cnm/deepdive/codebreaker/model/entity/Guess.java
+++ app/src/main/java/edu/cnm/deepdive/codebreaker/model/entity/Guess.java
@@ -1,7 +1,10 @@
 package edu.cnm.deepdive.codebreaker.model.entity;
 
+import android.annotation.SuppressLint;
+import androidx.annotation.NonNull;
 import com.google.gson.annotations.Expose;
 import java.util.Date;
+import java.util.Locale;
 
 public class Guess {
 
@@ -69,6 +72,13 @@ public class Guess {
 
   public void setSolution(boolean solution) {
     this.solution = solution;
+  }
+
+  @SuppressLint("DefaultLocale")
+  @NonNull
+  @Override
+  public String toString() {
+    return String.format("%1$s (%2$d / %3$d)", text, exactMatches, nearMatches);
   }
 
 }
