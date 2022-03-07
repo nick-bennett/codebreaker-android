d8ecf0125d7c434b090b94a0c5a15e6a75af2191
Nick Bennett <nick@nickbenn.com>
2022-03-07 13:46:13 -0700

    Basic ranking screen working.

--- app/src/main/java/edu/cnm/deepdive/codebreaker/controller/SummaryFragment.java
+++ app/src/main/java/edu/cnm/deepdive/codebreaker/controller/SummaryFragment.java
@@ -12,14 +12,16 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 import edu.cnm.deepdive.codebreaker.R;
 import edu.cnm.deepdive.codebreaker.databinding.FragmentSummaryBinding;
+import edu.cnm.deepdive.codebreaker.model.view.GameSummary;
 import edu.cnm.deepdive.codebreaker.viewmodel.GameViewModel;
+import edu.cnm.deepdive.codebreaker.viewmodel.StatisticsViewModel;
 
 public class SummaryFragment extends Fragment implements OnSeekBarChangeListener {
 
   private static final float MILLISECONDS_PER_SECOND = 1000.0f;
   private static final int SECONDS_PER_MINUTE = 60;
 
-  private GameViewModel viewModel;
+  private StatisticsViewModel viewModel;
   private FragmentSummaryBinding binding;
 
   public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -32,29 +34,10 @@ public class SummaryFragment extends Fragment implements OnSeekBarChangeListener
   public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
     super.onViewCreated(view, savedInstanceState);
     //noinspection ConstantConditions
-    viewModel = new ViewModelProvider(getActivity()).get(GameViewModel.class);
-    getLifecycle().addObserver(viewModel);
+    viewModel = new ViewModelProvider(getActivity()).get(StatisticsViewModel.class);
     viewModel
         .getSummary()
-        .observe(getViewLifecycleOwner(), (summary) -> {
-          if (summary == null) {
-            binding.countGames.setText(String.valueOf(0));
-            binding.minGuesses.setText("");
-            binding.maxGuesses.setText("");
-            binding.avgGuesses.setText("");
-            binding.minTime.setText("");
-            binding.maxTime.setText("");
-            binding.avgTime.setText("");
-          } else {
-            binding.countGames.setText(String.valueOf(summary.getCountGames()));
-            binding.minGuesses.setText(String.valueOf(summary.getMinGuesses()));
-            binding.maxGuesses.setText(String.valueOf(summary.getMaxGuesses()));
-            binding.avgGuesses.setText(getString(R.string.avg_guess_format, summary.getAvgGuesses()));
-            binding.minTime.setText(getDuration(summary.getMinTime()));
-            binding.maxTime.setText(getDuration(summary.getMaxTime()));
-            binding.avgTime.setText(getDuration((int) Math.round(summary.getAvgTime())));
-          }
-        });
+        .observe(getViewLifecycleOwner(), this::handleSummary);
     viewModel
         .getLength()
         .observe(getViewLifecycleOwner(), binding.length::setProgress);
@@ -89,6 +72,26 @@ public class SummaryFragment extends Fragment implements OnSeekBarChangeListener
     int minutes = seconds / SECONDS_PER_MINUTE;
     seconds %= SECONDS_PER_MINUTE;
     return getString(R.string.mmss_format, minutes, seconds);
+  }
+
+  private void handleSummary(GameSummary summary) {
+    if (summary == null) {
+      binding.countGames.setText(String.valueOf(0));
+      binding.minGuesses.setText("");
+      binding.maxGuesses.setText("");
+      binding.avgGuesses.setText("");
+      binding.minTime.setText("");
+      binding.maxTime.setText("");
+      binding.avgTime.setText("");
+    } else {
+      binding.countGames.setText(String.valueOf(summary.getCountGames()));
+      binding.minGuesses.setText(String.valueOf(summary.getMinGuesses()));
+      binding.maxGuesses.setText(String.valueOf(summary.getMaxGuesses()));
+      binding.avgGuesses.setText(getString(R.string.avg_guess_format, summary.getAvgGuesses()));
+      binding.minTime.setText(getDuration(summary.getMinTime()));
+      binding.maxTime.setText(getDuration(summary.getMaxTime()));
+      binding.avgTime.setText(getDuration((int) Math.round(summary.getAvgTime())));
+    }
   }
 
 }
